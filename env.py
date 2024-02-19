b395eb5f1d5053d410c5700c9dc5185222fc3642
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-02-19 18:30:28 +0000

    Create PostgresSQL

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "<postgres://uqjblnv8ooz:X3ppY4Ah97dm@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/class_quota_genre_673893>")
+    "DATABASE_URL", "postgres://uqjblnv8ooz:X3ppY4Ah97dm@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/class_quota_genre_673893")
