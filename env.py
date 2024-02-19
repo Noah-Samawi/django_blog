06fa817213afe5b6e6a3e3129335764790a13d10
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-02-19 19:16:12 +0000

    Initial commit

--- env.py
+++ env.py
@@ -1,4 +1,11 @@
-import os
+import osimport os
+
+# Other settings...
+
+# Static files (CSS, JavaScript, images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://uqjblnv8ooz:X3ppY4Ah97dm@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/class_quota_genre_673893")
