8d51267bd4861055c1826eef34cc3cfd430fc5e9
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-02-20 05:52:44 +0000

    Initial commit

--- env.py
+++ env.py
@@ -1,11 +1,11 @@
-import osimport os
+import os
 
 # Other settings...
 
 # Static files (CSS, JavaScript, images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://uqjblnv8ooz:X3ppY4Ah97dm@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/class_quota_genre_673893")
+import os
+
+os.environ.setdefault("SECRET_KEY", "543907")
