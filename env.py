ccf85d88dae6b7783a0f9da1144642370a0219f8
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-02-20 13:54:27 +0000

    Initail commit

--- env.py
+++ env.py
@@ -5,7 +5,7 @@ import os
 # Static files (CSS, JavaScript, images)
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgres://uqjblnv8ooz:X3ppY4Ah97dm@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/class_quota_genre_673893")
+    "DATABASE_URL", "postgres://gazhqxww:zF090-TY-hKlkdoTCJdFq1xbxs3gE2Mz@tai.db.elephantsql.com/gazhqxww")
 import os
 
 os.environ.setdefault("SECRET_KEY", "543907")
